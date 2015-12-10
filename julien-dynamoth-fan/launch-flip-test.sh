fef44e138a1d1b35bdc6244078e8c3da68e45a75
Fan Jin <im.fan.jin@gmail.com>
2015-12-10 02:33:38 -0500

    Add script to run tests

---
+++ julien-dynamoth-fan/launch-flip-test.sh
@@ -0,0 +1,23 @@
+./e-launch-cmd.sh cacheext-testimage flip 1000 flip-3MB-testimage- "/home/2012/fjin1/Desktop/CacheWise/images/3MB.jpg"
+
+sleep 5m
+
+./e-launch-cmd.sh cacheext-testimage flip 1000 flip-4.1MB-testimage- "/home/2012/fjin1/Desktop/CacheWise/images/4.1MB.jpg"
+
+sleep 5m
+
+./e-launch-cmd.sh cacheext-testimage flip 1000 flip-5.5MB-testimage- "/home/2012/fjin1/Desktop/CacheWise/images/5.5MB.jpg"
+
+sleep 5m
+
+./e-launch-cmd.sh cacheext-testimage flip 1000 flip-6.7MB-testimage- "/home/2012/fjin1/Desktop/CacheWise/images/6.7MB.jpg"
+
+sleep 5m
+
+./e-launch-cmd.sh cacheext-testimage flip 1000 flip-9.1MB-testimage- "/home/2012/fjin1/Desktop/CacheWise/images/9.1MB.jpg"
+
+sleep 5m
+
+./e-launch-cmd.sh cacheext-testimage flip 1000 flip-17.9MB-testimage- "/home/2012/fjin1/Desktop/CacheWise/images/17.9MB.jpg"
+
+sleep 1m
