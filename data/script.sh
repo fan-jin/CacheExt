d115b8068e37b927f5ad5dc649c623a75e788066
Fan Jin <im.fan.jin@gmail.com>
2015-12-10 16:58:26 -0500

    Add col label and filter to server bandwidth

--- data/script.sh
+++ data/script.sh
@@ -3,8 +3,9 @@
 for f in ./*/*/*.csv; do
   case $f in
       *-*mb-[0-9]*_*.csv)
-       # echo $f
-      sed -i -e '1,40d' "$f"
+       # echo "$f"
+#      sed -i -e '1,40d' "$f"
+       echo "`cat ./label-and-filter.csv "$f"`" > "$f"
         ;;
   esac
 done
