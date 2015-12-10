03f0fbc5ea6f2dfdedf52545a970decac729a300
Fan Jin <im.fan.jin@gmail.com>
2015-12-10 17:00:27 -0500

    Add col label and filter to client bandwidth

--- data/script.sh
+++ data/script.sh
@@ -2,7 +2,7 @@
 
 for f in ./*/*/*.csv; do
   case $f in
-      *-*mb-[0-9]*_*.csv)
+      *-*MB-[0-9]*_*.csv)
        # echo "$f"
 #      sed -i -e '1,40d' "$f"
        echo "`cat ./label-and-filter.csv "$f"`" > "$f"
